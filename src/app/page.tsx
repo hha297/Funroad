167a90cb788dbab1db3081a21cd27c16a8333df9
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-05-07 10:10:38 +0300

    Component customize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
-  return <div className="text-red-600 text-lg">Hello</div>;
+  return (
+    <div className="flex flex-col gap-y-4 px-4">
+      <div>
+        {/* Add full variants of shadcn */}
+
+        <Button variant="elevated">Button</Button>
+      </div>
+      <div>
+        <Input placeholder="Input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
+  );
 }
