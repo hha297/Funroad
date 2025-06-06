056a620f947d9963cc85fef6bdd035fc9cebf6a1
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-06-06 09:47:39 +0300

    Fix routing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
-        // In Development, use normal path
-        if (process.env.NODE_ENV === 'development') {
+        const isDevelopment = process.env.NODE_ENV === 'development';
+        const isSubdomainRoutingEnabled = process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === 'true';
+
+        // In Development or if subdomain routing is disabled, use normal path
+        if (isDevelopment || !isSubdomainRoutingEnabled) {
                 return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
         }
 
