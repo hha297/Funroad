cd54f5e7de47fead1e08049d54e0f74bb0c79b36
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-06-09 10:03:55 +0300

    Fix suspense

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,10 @@
 import { DEFAULT_LIMIT } from '@/lib/constants';
+import { ProductListSkeleton } from '@/modules/products/ui/components/product-list';
 
 import { ProductListView } from '@/modules/products/ui/views/product-list-view';
 import { getQueryClient, trpc } from '@/trpc/server';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 export default async function Home() {
         const queryClient = getQueryClient();
@@ -15,7 +17,9 @@ export default async function Home() {
 
         return (
                 <HydrationBoundary state={dehydrate(queryClient)}>
-                        <ProductListView />
+                        <Suspense fallback={<ProductListSkeleton />}>
+                                <ProductListView />
+                        </Suspense>
                 </HydrationBoundary>
         );
 }
